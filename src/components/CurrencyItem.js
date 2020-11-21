a09313a6c6a409a2c741c08ce2d89acbfe07fb54
rednaxela700 <ohleg@windowslive.com>
2020-11-21 19:42:11 +0100

    added styles to mocked CurrencyItem data

--- src/components/CurrencyItem.js
+++ src/components/CurrencyItem.js
@@ -1,6 +1,24 @@
 import React from 'react';
-import { Segment } from 'semantic-ui-react';
+import { Segment, Button } from 'semantic-ui-react';
 
+const styles = {
+  container: { display: 'flex', justifyContent: 'space-between', alignItems: 'center' },
+  currencyName: { minWidth: '25%', margin: '0' },
+  price: { minWidth: '100px' },
+  btn: { minWidth: '93px' },
+};
 export default function CurrencyItem() {
-  return <Segment attached>currency</Segment>;
+  return (
+    <Segment attached>
+      <div style={styles.container}>
+        <p style={styles.currencyName}>Currency</p>
+        <div style={styles.price}>
+          <span>100zł</span>
+          {' / '}
+          <span>3000zł</span>
+        </div>
+        <Button positive>Add</Button>
+      </div>
+    </Segment>
+  );
 }
