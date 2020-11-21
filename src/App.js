3ec2bd0920b7ca9e77d835d77a173cd8fd6b4742
rednaxela700 <ohleg@windowslive.com>
2020-11-21 18:22:58 +0100

    added routing to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
+import { ROUTE_FAVOURITES, ROUTE_MAIN } from './routes';
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path={ROUTE_MAIN}></Route>
+      <Route exact path={ROUTE_FAVOURITES}></Route>
+      <Redirect to="/" />
+    </Switch>
+  </Router>
+);
 
 export default App;
