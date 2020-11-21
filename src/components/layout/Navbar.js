40ccf7ac2246a2002dc4ec431a28e5de1d108af7
rednaxela700 <ohleg@windowslive.com>
2020-11-21 18:41:17 +0100

    added header and nav, no styles

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import { Header, Icon, Menu } from 'semantic-ui-react';
+import { ROUTE_FAVOURITES, ROUTE_MAIN } from '../../routes';
 
 export default function Navbar() {
-  return <div></div>;
+  const styles = {
+    header: { padding: '1rem' },
+    nav: { marginTop: '0' },
+  };
+  return (
+    <Fragment>
+      <Header as="h1" styles={styles.header}>
+        <Icon name="dollar sign" />
+        Currency App
+      </Header>
+      <Menu as="nav" attached="top" tabular style={styles.nav}>
+        <Menu.Item as={Link} to={ROUTE_MAIN}>
+          Main
+        </Menu.Item>
+        <Menu.Item as={Link} to={ROUTE_FAVOURITES}>
+          Favourites
+        </Menu.Item>
+      </Menu>
+    </Fragment>
+  );
 }
