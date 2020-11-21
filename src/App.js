a04d4eb8423fcabd9e1e2a9b0b8305e3d8515476
rednaxela700 <ohleg@windowslive.com>
2020-11-21 18:29:20 +0100

    added boilerplate components to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Favourites from './components/pages/Favourites';
+import Main from './components/pages/Main';
 import { ROUTE_FAVOURITES, ROUTE_MAIN } from './routes';
 
 const App = () => (
   <Router>
+    <Navbar />
     <Switch>
-      <Route exact path={ROUTE_MAIN}></Route>
-      <Route exact path={ROUTE_FAVOURITES}></Route>
+      <Route exact path={ROUTE_MAIN}>
+        <Main />
+      </Route>
+      <Route exact path={ROUTE_FAVOURITES}>
+        <Favourites />
+      </Route>
       <Redirect to="/" />
     </Switch>
   </Router>
