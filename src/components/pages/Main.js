82ffaf20aeee438c476e033af9f0fe6ae23ce08d
rednaxela700 <ohleg@windowslive.com>
2020-11-21 18:54:17 +0100

    set basic layout in Main

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import { Container, Header, Grid, Segment } from 'semantic-ui-react';
 
+export const mainStyles = {
+  container: { padding: '5em 0em' },
+  header: { display: 'flex', justifyContent: 'space-between', alignItems: 'center' },
+};
 export default function Main() {
-  return <div></div>;
+  return (
+    <Container as="section" style={mainStyles.container}>
+      <Header as="h1">Currencies:</Header>
+      <Grid.Column>
+        <Segment attached>currency</Segment>
+      </Grid.Column>
+    </Container>
+  );
 }
