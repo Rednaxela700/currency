88a35cbcd391e87ad78fc3358ce3dfa7ce3d55bf
rednaxela700 <ohleg@windowslive.com>
2020-11-21 19:29:34 +0100

    moved Main fetched data state to context lvl state

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,27 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Header, Grid, Segment } from 'semantic-ui-react';
+import React, { useContext, useEffect } from 'react';
+import { Container, Header, Grid } from 'semantic-ui-react';
 import { FETCH_RATES } from '../../api';
 import useDataFetching from '../../hooks/useDataFetch';
 import CurrencyItem from '../CurrencyItem';
+import CurrenciesContext from '../../context/currencies/currenciesContext';
 
 export const mainStyles = {
   container: { padding: '5em 0em' },
   header: { display: 'flex', justifyContent: 'space-between', alignItems: 'center' },
 };
 export default function Main() {
-  const [data, setData] = useState(null);
+  const currenciesContext = useContext(CurrenciesContext);
+  const { setCurrencies, currencies } = currenciesContext;
 
   const { loading, results } = useDataFetching(`${process.env.REACT_APP_API_KEY}${FETCH_RATES}`);
   useEffect(() => {
-    setData(results);
+    if (!currencies) {
+      setCurrencies(results[0]);
+    }
   }),
     [];
   if (loading) return null;
+
+  const { rates } = currencies;
   return (
     <Container as="section" style={mainStyles.container}>
       <Header as="h1">Currencies:</Header>
       <Grid.Column>
-        {data[0].rates.map((currency) => (
+        {rates.map((currency) => (
           <CurrencyItem key={currency.code} />
         ))}
       </Grid.Column>
