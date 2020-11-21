00d719122f55d1387a64de82aa9818380650706e
rednaxela700 <ohleg@windowslive.com>
2020-11-21 21:40:34 +0100

    render add/remove btn conditionally

--- src/components/CurrencyItem.js
+++ src/components/CurrencyItem.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Segment, Button } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
+import CurrenciesContext from '../context/currencies/currenciesContext';
 
 const styles = {
   container: { display: 'flex', justifyContent: 'space-between', alignItems: 'center' },
@@ -11,9 +12,28 @@ const styles = {
 export const firstLetterToUppercase = (string) => string.charAt(0).toUpperCase() + string.slice(1);
 
 export default function CurrencyItem({ data: { currency, code, ask, bid }, handleSubmit }) {
+  const currenciesContext = useContext(CurrenciesContext);
+  const { favourites } = currenciesContext;
+
   const currencyName = firstLetterToUppercase(currency);
   const sellPrice = ask.toFixed(2);
   const buyPrice = bid.toFixed(2);
+
+  const CurrentBtn = () => {
+    if (favourites && favourites.find((favedCurrency) => favedCurrency.code === code)) {
+      return (
+        <Button style={styles.btn} type="submit" negative>
+          Remove
+        </Button>
+      );
+    }
+    return (
+      <Button style={styles.btn} type="submit" positive>
+        Add
+      </Button>
+    );
+  };
+
   return (
     <Segment attached>
       <form
@@ -31,9 +51,7 @@ export default function CurrencyItem({ data: { currency, code, ask, bid }, handl
           {' / '}
           <span>{buyPrice}zł</span>
         </div>
-        <Button type="submit" style={styles.btn} positive>
-          Add
-        </Button>
+        <CurrentBtn />
       </form>
     </Segment>
   );
