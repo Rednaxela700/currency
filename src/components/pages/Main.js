6d057ef16249da139a5a2533fd5659fd2d0a7a87
rednaxela700 <ohleg@windowslive.com>
2020-11-21 19:05:36 +0100

    fetched data in Main component

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Header, Grid, Segment } from 'semantic-ui-react';
+import { FETCH_RATES } from '../../api';
+import useDataFetching from '../../hooks/useDataFetch';
 
 export const mainStyles = {
   container: { padding: '5em 0em' },
   header: { display: 'flex', justifyContent: 'space-between', alignItems: 'center' },
 };
 export default function Main() {
+  const [data, setData] = useState(null);
+
+  const { loading, results } = useDataFetching(`${process.env.REACT_APP_API_KEY}${FETCH_RATES}`);
+  useEffect(() => {
+    setData(results);
+  }),
+    [];
+  if (loading) return null;
   return (
     <Container as="section" style={mainStyles.container}>
       <Header as="h1">Currencies:</Header>
