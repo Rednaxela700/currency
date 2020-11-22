4de7cb0897386140499496f72b5d8edc23fae870
rednaxela700 <ohleg@windowslive.com>
2020-11-22 15:30:30 +0100

    added Favourites header and clearAll btn

--- src/components/pages/Favourites.js
+++ src/components/pages/Favourites.js
@@ -2,6 +2,7 @@ import React, { useContext, Fragment } from 'react';
 import { Header, Container } from 'semantic-ui-react';
 import CurrenciesContext from '../../context/currencies/currenciesContext';
 import ModalContext from '../../context/modal/modalContext';
+import ClearBtn from '../ClearBtn';
 import CurrencyItem from '../CurrencyItem';
 import Modal from '../Modal';
 import { mainStyles } from './Main';
@@ -24,14 +25,29 @@ export default function Favourites() {
     const updatedFavourites = favourites.filter((removedCurr) => removedCurr.code !== currencyCode);
     setFavourites(updatedFavourites);
   };
+  const HeaderWithFavourites = () => (
+    <header style={{ ...mainStyles.header, marginBottom: '1em' }}>
+      <Header as="h2" style={mainStyles.heading} content="Your favourite currencies" />
+      <p style={mainStyles.prices}>Buy/Sell</p>
+      <div style={mainStyles.btnContainer}>
+        <ClearBtn />
+      </div>
+    </header>
+  );
+
+  const HeaderWithOutFavourites = () => (
+    <header style={{ ...mainStyles.header, marginBottom: '1em' }}>
+      <Header as="h2" style={mainStyles.heading} content="No favourites retrieved, add some." />
+      <p style={mainStyles.prices}> </p>
+      <div style={mainStyles.btnContainer}>
+        <ClearBtn />
+      </div>
+    </header>
+  );
   return (
     <Fragment>
       <Container style={mainStyles.container}>
-        <Header as="h1">
-          {favourites.length == 0
-            ? 'No favourites retrieved, add some.'
-            : 'Your favourite currencies'}
-        </Header>
+        {favourites.length == 0 ? <HeaderWithOutFavourites /> : <HeaderWithFavourites />}
         {favourites.map((currency) => (
           <CurrencyItem key={currency.currency} data={currency} handleSubmit={handleFavourite} />
         ))}
